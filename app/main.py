eecc59e03ba41a4f4fe624a7c45fe546e21a413e
EagIe0wI <anya230704@gmail.com>
2026-09-20 17:13:41 +0300

    added /test-booking, fixed create_booking in booking_service

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.schemas.booking import BookingCreate
+from app.services.booking_service import BookingService
 
 app = FastAPI(title="Mise Restaurant Booking API")
 
@@ -10,3 +11,8 @@ async def root():
 @app.post("/test-validation")
 async def test_validation(booking: BookingCreate):
     return {"message": "Данные успешно прошли валидацию Pydantic!", "data": booking}
+
+@app.post("/test-booking")
+async def test_booking(booking: BookingCreate):
+    saved_booking = BookingService.create_booking(booking)
+    return saved_booking
--- app/services/booking_service.py
+++ app/services/booking_service.py
@@ -4,26 +4,20 @@ from app.schemas.booking import BookingCreate
 from fastapi import HTTPException
 
 db_bookings: list[Booking] = []
-
 _id_counter = 1
 
 class BookingService:
     @classmethod
     def create_booking(cls, booking_data: BookingCreate) -> Booking:
         global _id_counter
-        
-        @classmethod
-        def create_booking(cls, booking_data: BookingCreate) -> Booking:
-            global _id_counter
-            
-            for existing_booking in db_bookings:
-                if existing_booking.status == BookingStatus.ACTIVE:
-                    if (existing_booking.booking_date == booking_data.booking_date and 
-                        existing_booking.booking_time == booking_data.booking_time):
-                        raise HTTPException(
-                            status_code=409, 
-                            detail="Слот на выбранные дату и время уже занят"
-                        )
+        for existing_booking in db_bookings:
+            if existing_booking.status == BookingStatus.ACTIVE:
+                if (existing_booking.booking_date == booking_data.booking_date and 
+                    existing_booking.booking_time == booking_data.booking_time):
+                    raise HTTPException(
+                        status_code=409, 
+                        detail="Слот на выбранные дату и время уже занят"
+                    )
         
         new_booking = Booking(
             id=_id_counter,
