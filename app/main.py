d8445d61b166a20b4cfad02f4a856dc9cf3fdac9
EagIe0wI <anya230704@gmail.com>
2026-09-20 17:38:43 +0300

    documented 404 and 409 errors in swagger ui

--- app/main.py
+++ app/main.py
@@ -1,30 +1,55 @@
 from datetime import date
-from fastapi import FastAPI
-from app.schemas.booking import BookingCreate
+from fastapi import FastAPI, Query
+from pydantic import BaseModel, Field
+from app.schemas.booking import BookingCreate, BookingOut
 from app.services.booking_service import BookingService
 
 app = FastAPI(title="Mise Restaurant Booking API")
 
-@app.get("/")
-async def root():
-    return {"status": "working", "message": "Hello World from FastAPI"}
+class ErrorResponse(BaseModel):
+    detail: str = Field(..., examples=["Описание ошибки со стороны сервера"])
 
-# Тест создания брони (POST)
-@app.post("/test/bookings")
+# Создание брони (POST)
+@app.post(
+    "/test/bookings", 
+    response_model=BookingOut, 
+    status_code=201,
+    responses={
+        409: {"model": ErrorResponse, "description": "Слот на выбранные дату/время уже занят"}
+    }
+)
 async def test_create(booking: BookingCreate):
     return BookingService.create_booking(booking)
 
-# Тест получения списка с фильтром (GET)
-@app.get("/test/bookings")
-async def test_get_all(date: date | None = None):
+# Получение списка с фильтром (GET)
+@app.get(
+    "/test/bookings", 
+    response_model=list[BookingOut],
+    responses={
+        422: {"description": "Невалидный формат переданной даты"}
+    }
+)
+async def test_get_all(date: date | None = Query(None, description="Фильтр по дате в формате YYYY-MM-DD")):
     return BookingService.get_bookings(date)
 
-# Тест получения одной брони по ID (GET)
-@app.get("/test/bookings/{booking_id}")
+# Получение одной брони по ID (GET)
+@app.get(
+    "/test/bookings/{booking_id}", 
+    response_model=BookingOut,
+    responses={
+        404: {"model": ErrorResponse, "description": "Бронь с указанным ID не найдена"}
+    }
+)
 async def test_get_one(booking_id: int):
     return BookingService.get_booking_by_id(booking_id)
 
-# Тест отмены брони (DELETE)
-@app.delete("/test/bookings/{booking_id}")
+# Отмена брони (DELETE)
+@app.delete(
+    "/test/bookings/{booking_id}", 
+    response_model=BookingOut,
+    responses={
+        404: {"model": ErrorResponse, "description": "Бронь с указанным ID не найдена для отмены"}
+    }
+)
 async def test_cancel(booking_id: int):
     return BookingService.cancel_booking(booking_id)
