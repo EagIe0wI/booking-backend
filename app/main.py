1ee7d16cd87238c86cd0ef433ff430e73829e64f
EagIe0wI <anya230704@gmail.com>
2026-09-24 13:11:32 +0300

    update docker execution commands with detached flag

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.api.bookings import router as bookings_router
 
 app = FastAPI(title="Mise Restaurant Booking API", version="1.0.0")
@@ -6,8 +6,10 @@ app = FastAPI(title="Mise Restaurant Booking API", version="1.0.0")
 app.include_router(bookings_router)
 
 @app.get("/")
-async def root():
+async def root(request: Request):
+    base_url = str(request.base_url)
     return {
         "status": "working", 
-        "message": "Mise Restaurant Booking API ис успешно запущено и готово к работе."
+        "message": "Mise Restaurant Booking API успешно запущено и готово к работе.",
+        "documentation": f"{base_url}docs"
     }
